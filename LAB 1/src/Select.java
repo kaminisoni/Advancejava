cfa5514df9854b6ecfe92b4d94ae04c2221c8341
Kamini Chourasia <109265765+kaminisoni@users.noreply.github.com>
2024-03-08 15:17:49 +0530

    Update Select.java

--- LAB 1/src/Select.java
+++ LAB 1/src/Select.java
@@ -6,7 +6,7 @@ public class Select
 	{
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/BCA","root","Aryan@04");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/BCA","root","Kamini@22");
 		
 		Statement st = con.createStatement();
 		
